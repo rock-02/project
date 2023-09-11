178808f2b49d274d7366c2f043f405afb36b6350
tarun_maidur <tarunmaidur@gmail.com>
2023-09-11 10:43:41 +0530

    commit

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,8 +3,14 @@ const {
   signUp,
   signIn,
   mailOtp,
-  updatePassword,
   forgotPassword,
+  resetPassoword,
+  changePassword,
+  complteProfile,
+  getAllusers,
+  getSingleUser,
+  updateRole,
+  deleteUser,
 } = require("../controller/userController");
 const { route } = require("../app");
 const isLoggedIn = require("../middlewares/isloggedIn");
@@ -17,10 +23,28 @@ router.post("/signin", signIn);
 
 router.post("/regester", mailOtp);
 
-router.post("/isloggin", isLoggedIn);
-
-// router.post("/update", isLoggedIn, updatePassword);
-
 router.post("/forgotpassword", forgotPassword);
+
+router.put("/reset/:token", resetPassoword);
+
+router.route("/updatepassword").put(isLoggedIn, changePassword);
+
+router.route("/compteprofile", isLoggedIn, complteProfile);
+
+router
+  .route("/admin/users")
+  .get(isLoggedIn, isAuthorizedRoles("adimn"), getAllusers);
+
+router
+  .route("/admin/user/:id")
+  .get(isLoggedIn, isAuthorizedRoles("adimn"), getSingleUser);
+
+router
+  .route("/admin/user/:id")
+  .get(isLoggedIn, isAuthorizedRoles("adimn"), updateRole);
+
+router
+  .route("/admin/user/:id")
+  .delete(isLoggedIn, isAuthorizedRoles("adimn"), deleteUser);
 
 module.exports = router;
